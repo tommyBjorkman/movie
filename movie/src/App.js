51b428dfebe48d63373084ef180b70fcf7584b81
tommyBjorkman <git-autoconfig>
2022-12-30 15:12:16 +0100

    fixed search!

--- movie/src/App.js
+++ movie/src/App.js
@@ -5,7 +5,9 @@ import MovieCard from './MovieCard';
 
 function App() {
   const api ="https://api.themoviedb.org/3/movie/popular?api_key=a2c742d9be4b7668fe4be70f4a0b6cc6"
+  const api_search ="https://api.themoviedb.org/3/search/movie?api_key=a2c742d9be4b7668fe4be70f4a0b6cc6&query="
   const [movies,setMovies] = useState([])
+  const [term, setTerm] = useState('')
 
   useEffect(() =>{
     fetch(api)
@@ -15,6 +17,13 @@ function App() {
 
 console.log(movies)
 
+const handleSearch = (e) =>{
+  e.preventDefault()
+  fetch(api_search + term)
+    .then(res => res.json())
+    .then(data => setMovies(data.results))
+}
+
   return (
     <div className="App">
       <div className='search_nav'>
@@ -22,8 +31,8 @@ console.log(movies)
           <h1>Movies</h1>
         </div>
         <div>
-          <form>
-            <input />
+          <form onSubmit={handleSearch}>
+            <input onChange={(e) => setTerm(e.target.value)} />
             <button>Search</button>
           </form>
         </div>
