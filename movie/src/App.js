80593cc36fcbe6fa8d99ead0c17c3a26b339f8d1
tommyBjorkman <git-autoconfig>
2022-12-30 16:34:03 +0100

    fixed moviedb

--- movie/src/App.js
+++ movie/src/App.js
@@ -43,6 +43,7 @@ const handleSearch = (e) =>{
             <MovieCard {...movie} />
         ))}
       </div>
+      <div className='moviedb'><a href="https://www.themoviedb.org/"><img src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_long_2-9665a76b1ae401a510ec1e0ca40ddcb3b0cfe45f1d51b77a308fea0845885648.svg"></img></a></div>
     </div>
   );
 }
