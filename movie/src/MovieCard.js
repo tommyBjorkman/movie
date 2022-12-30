c8f781c06e35295d31aa372418ad7ca223e256ec
tommyBjorkman <git-autoconfig>
2022-12-30 16:12:10 +0100

    added links to movies

--- movie/src/MovieCard.js
+++ movie/src/MovieCard.js
@@ -4,9 +4,10 @@ import './MovieCard.css';
 const MovieCard = (props) => {
 
     const api_img ="https://image.tmdb.org/t/p/w500/"
+    const url ="https://www.themoviedb.org/movie/"
 
     return (
-        <div className="card">
+        <div className="card"><a href={url + props.id}>
             <div className="poster">
                 <img src={props.poster_path ? api_img + props.poster_path : "https://images.pexels.com/photos/10718272/pexels-photo-10718272.jpeg"} />
             </div>
@@ -17,7 +18,7 @@ const MovieCard = (props) => {
             <div className="overview">
                 <h2 className="title_overview"></h2>
                 <h3 className="info_overview">{props.overview}</h3>
-            </div>
+            </div></a>
         </div>
     )
 }
